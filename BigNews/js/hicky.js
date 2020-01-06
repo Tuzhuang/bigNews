ef282e95c32b36c626ad91bde39622f7c80c866e
归少壮 <guishaozhaung@126.com>
2020-01-06 11:02:52 +0800

    完成前台的主页面

---
+++ BigNews/js/hicky.js
@@ -0,0 +1,22 @@
+// 使用闭包沙箱来保存请求接口的地址
+
+(function (w) {
+    // 创建保存变量的基地址
+    let baseURL = 'http://localhost:8080/api/v1';
+    // 创建对象来保存基地址
+    let BigNews = {
+        baseURL: baseURL,
+        hotpic: baseURL + '/index/hotpic',  // 首页焦点图
+        category: baseURL + '/index/category',  // 文章类型
+        latest: baseURL + '/index/latest',  // 最新资讯
+        rank: baseURL + '/index/rank',  // 热门排行
+        latest_comment: baseURL + '/index/latest_comment',  // 最新评论
+        attention: baseURL + '/index/attention',  // 焦点关注
+        get_comment: baseURL + '/index/get_comment',  // 评论列表
+        post_comment: baseURL + '/index/post_comment',  // 发表评论
+        search: baseURL + '/index/search'  // 文章搜索
+    }
+
+    // 将创建的BigNews对象暴露给window
+    window.BigNews = BigNews;
+})(window)
